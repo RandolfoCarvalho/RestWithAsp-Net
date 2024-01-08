9b5a8368c307ee5867210e92db0ea5c555b43add
RandolfoCarvalho <randolfoirades@gmail.com>
2024-01-07 22:49:12 -0300

    Add custom serialization

--- 02_RestWithAsp-NET_Calculator/RestWithAspNet/RestWithAspNet/Data/VO/PersonVO.cs
+++ 02_RestWithAsp-NET_Calculator/RestWithAspNet/RestWithAspNet/Data/VO/PersonVO.cs
@@ -1,12 +1,19 @@
 ﻿
+using System.Text.Json.Serialization;
+
 namespace RestWithAspNet.Data.VO
 {
+
     public class PersonVO
-    { 
+    {
+        [JsonPropertyName("code")]
         public long Id { get; set; }
-        public string FirstName { get; set; } 
+        [JsonPropertyName("name")]
+        public string FirstName { get; set; }
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; } 
         public string Adress { get; set; }
+        [JsonPropertyName("sex")]
         public string Gender { get; set; }
     }
 }
