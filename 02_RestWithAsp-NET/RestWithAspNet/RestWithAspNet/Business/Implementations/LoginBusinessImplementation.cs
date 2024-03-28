d41c4abe27d744c13d4944c330311dd7d05b1b92
RandolfoCarvalho <randolfoirades@gmail.com>
2024-03-27 23:46:35 -0300

    Implementing class VO and Business

---
+++ 02_RestWithAsp-NET/RestWithAspNet/RestWithAspNet/Business/Implementations/LoginBusinessImplementation.cs
@@ -0,0 +1,52 @@
+﻿using RestWithAspNet.Configurations;
+using RestWithAspNet.Data.VO;
+using RestWithAspNet.Repository;
+using RestWithAspNet.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace RestWithAspNet.Business.Implementations
+{
+    public class LoginBusinessImplementation : ILoginBusiness
+    {
+        private const string DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+        private TokenConfiguration _configuration;
+        private IUserRepository _repository;
+        private readonly ITokenService _tokenService;
+
+        public LoginBusinessImplementation(TokenConfiguration configuratin, IUserRepository repository, ITokenService tokenService)
+        {
+            _configuration = configuratin;
+            _tokenService = tokenService;
+            _repository = repository;
+        }
+
+        public TokenVO ValidateCredentials(UserVO userCredentials)
+        {
+            var user = _repository.ValidateCredentials(userCredentials);
+            if (user == null) return null;
+            var claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                new Claim(JwtRegisteredClaimNames.Jti, user.UserName)
+            };
+            var accessToken = _tokenService.GenerateAcessToken(claims);
+            var refreshToken = _tokenService.GenerateRefreshToken();
+
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiryTime = DateTime.Now.AddDays(_configuration.DaysToExpire);
+            DateTime createDate = DateTime.Now;
+            DateTime expirationDate = createDate.AddMinutes(_configuration.Minutes);
+
+
+            return new TokenVO
+                (
+                    true,
+                    createDate.ToString(DATE_FORMAT),
+                    expirationDate.ToString(DATE_FORMAT),
+                    accessToken,
+                    refreshToken
+                );
+        }
+    }
+}
---
+++ 02_RestWithAsp-NET/RestWithAspNet/RestWithAspNet/Data/VO/TokenVO.cs
@@ -0,0 +1,21 @@
+﻿namespace RestWithAspNet.Data.VO
+{
+    public class TokenVO
+    {
+        public TokenVO(bool authenticaded, string created, string expiration, string accessToken, string refreshToken )
+        {
+            Authenticaded = authenticaded;
+            Created = created;
+            Expiration = expiration;
+            AccessToken = accessToken;
+            RefreshToken = refreshToken;
+        }
+
+        public bool Authenticaded { get; set; }
+        public string Created { get; set; }
+        public string Expiration { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+
+    }
+}
