f995c0f54ed40556d36584df7be929e6bd05ed5a
randolfoCarvalho <randolfoirades@gmail.com>
2024-06-06 23:41:52 -0300

    Implementando PersonRepository

---
+++ 02_RestWithAsp-NET/RestWithAspNet/RestWithAspNet/Repository/IPersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using RestWithAspNet.Data.VO;
+using RestWithAspNet.Model;
+
+namespace RestWithAspNet.Repository
+{
+    public interface IUserRepository
+    {
+        User ValidateCredentials(UserVO user);
+        User ValidateCredentials(string userName);
+
+        bool RevokeToken(string userName);
+        User RefreshUserInfo(User info);
+    }
+}
---
+++ 02_RestWithAsp-NET/RestWithAspNet/RestWithAspNet/Repository/PersonRepository.cs
@@ -0,0 +1,32 @@
+﻿using RestWithAspNet.Repository.Generic;
+using RestWithAspNet.Model;
+using RestWithAspNet.Data;
+
+namespace RestWithAspNet.Repository
+{
+    public class PersonRepository : GenericRepository<Person>, IPersonRepository
+    {
+        public PersonRepository(MysqlContext context) : base(context) 
+        {
+
+        }
+        public Person Disable(long id)
+        {
+            if (_context.Persons.Any(p => p.Id.Equals(id))) return null;
+            var user = _context.Persons.FirstOrDefault(p => p.Id.Equals(id));
+            if(user != null)
+            {
+                user.Enabled = false;
+            }
+            try
+            {
+                _context.Entry(user).CurrentValues.SetValues(user);
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception(ex.Message);
+            }
+            return user;
+        }
+    }
+}
